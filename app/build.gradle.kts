a106f5fcd3fb68a13e5b8123be4d8ee5c1d6be2d
the-uncanny-valley <runcannyvalley@gmail.com>
2025-10-31 21:27:07 +0300

    build(deps): add coroutines support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,5 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.coroutines.core)
 }
