abeeb2708c83a7e5b77ece7d21628e3c2c555fac
the-uncanny-valley <runcannyvalley@gmail.com>
2025-10-31 20:41:06 +0300

    feat(domain): implement VPN use cases
    - Create use case classes for connect, disconnect, and get status operations
    - Each use case delegates to VpnRepository with operator invoke
    - Provides clean API for presentation layer to interact with VPN functionality

---
+++ app/src/main/java/com/uncannyvalley/astralvpn/domain/usecase/ConnectVpnUseCase.kt
@@ -0,0 +1,17 @@
+package com.uncannyvalley.astralvpn.domain.usecase
+
+import com.uncannyvalley.astralvpn.domain.model.VpnConfig
+import com.uncannyvalley.astralvpn.domain.model.VpnStatus
+import com.uncannyvalley.astralvpn.domain.repository.VpnRepository
+
+class ConnectVpnUseCase(private val vpnRepository: VpnRepository) {
+    suspend operator fun invoke(config: VpnConfig) = vpnRepository.connect(config)
+}
+
+class DisconnectVpnUseCase(private val vpnRepository: VpnRepository) {
+    suspend operator fun invoke() = vpnRepository.disconnect()
+}
+
+class GetVpnUseCase(private val vpnRepository: VpnRepository) {
+    suspend operator fun invoke(): VpnStatus = vpnRepository.getStatus()
+}
