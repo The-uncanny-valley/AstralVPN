aa3b0040d9e5e01164c428aa5c3c9d51e7e6c807
the-uncanny-valley <runcannyvalley@gmail.com>
2025-11-25 11:19:53 +0300

    feat(nav): add AppRoutes for defining navigation destinations

---
+++ app/src/main/java/com/uncannyvalley/astralvpn/presentation/navigation/AppRoutes.kt
@@ -0,0 +1,44 @@
+package com.uncannyvalley.astralvpn.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.uncannyvalley.astralvpn.presentation.home.HomeScreen
+import com.uncannyvalley.astralvpn.presentation.home.HomeUiState
+import com.uncannyvalley.astralvpn.presentation.home.HomeViewModel
+import com.uncannyvalley.astralvpn.presentation.settings.SettingsScreen
+
+sealed class Screen(val route: String) {
+    object HomeScreen : Screen("home")
+    object SettingsScreen : Screen("settings")
+}
+
+@Composable
+fun SettingsRoute(
+    onBack: () -> Unit
+) {
+    SettingsScreen(onBack = onBack)
+}
+
+@Composable
+fun HomeRoute(
+    onNavigateSettings: () -> Unit,
+    viewModel: HomeViewModel = hiltViewModel()
+) {
+    val uiState by viewModel.uiState.collectAsState()
+    
+    HomeScreen(
+        uiState = uiState,
+        onMainButtonClick = {
+            when (uiState) {
+                is HomeUiState.Normal -> viewModel.onConnectClicked()
+                is HomeUiState.Connected,
+                is HomeUiState.Connecting -> viewModel.onDisconnectClicked()
+                is HomeUiState.Error -> viewModel.onDisconnectClicked()
+                HomeUiState.NoInternet -> viewModel.refreshConnectionStatus()
+            }
+        },
+        onNavigateSettings = onNavigateSettings
+    )
+}
