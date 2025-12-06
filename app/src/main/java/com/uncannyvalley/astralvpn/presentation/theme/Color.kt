cd56b0d3b5bdbc8e210757381e1d24a6921147c0
The-uncanny-valley <runcannyvalley@gmail.com>
2025-12-06 20:27:59 +0300

    chore(ui): remove unused colors from Color.kt

--- app/src/main/java/com/uncannyvalley/astralvpn/presentation/theme/Color.kt
+++ app/src/main/java/com/uncannyvalley/astralvpn/presentation/theme/Color.kt
@@ -2,10 +2,6 @@ package com.uncannyvalley.astralvpn.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
 val Puce = Color(0xFF846582)
 val EerieBlack = Color(0xFF171D1B)
 val SilverSand = Color(0xFFC4C4C4)
