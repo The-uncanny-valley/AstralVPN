1dfab191beab71f496926b62b515b1100818f9dd
The-uncanny-valley <runcannyvalley@gmail.com>
2025-12-07 17:17:59 +0300

    feat(ui): add Help screen with preview

---
+++ app/src/main/java/com/uncannyvalley/astralvpn/presentation/screen/HelpScreen.kt
@@ -0,0 +1,110 @@
+package com.uncannyvalley.astralvpn.presentation.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.uncannyvalley.astralvpn.R
+import com.uncannyvalley.astralvpn.presentation.theme.AstralVPNTheme
+
+@Composable
+fun HelpScreen(
+    onBack: () -> Unit
+) {
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) { padding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .paint(
+                    painterResource(id = R.drawable.background_stars),
+                    contentScale = ContentScale.FillHeight
+                )
+                .padding(padding) // ?
+        ) {
+            // glass
+            Box(
+                modifier = Modifier
+                    .padding(start = 24.dp, end = 24.dp, top = 112.dp, bottom = 86.dp)
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(16.dp))
+                    .paint(
+                        painterResource(id = R.drawable.background_glass),
+                        contentScale = ContentScale.FillBounds
+                    ),
+                contentAlignment = Alignment.TopStart
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .verticalScroll(rememberScrollState())
+                        .clip(RoundedCornerShape(16.dp))
+                        .padding(horizontal = 42.dp, vertical = 48.dp),
+                    verticalArrangement = Arrangement.Top
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.btn_return),
+                        contentDescription = null,
+                        tint = Color.Unspecified,
+                        modifier = Modifier
+                    )
+
+                    Spacer(modifier = Modifier.height(28.dp))
+
+                    Text(
+                        text = stringResource(id = R.string.help_title),
+                        style = MaterialTheme.typography.titleMedium,
+                        color = MaterialTheme.colorScheme.onPrimary,
+                    )
+
+                    Spacer(modifier = Modifier.height(32.dp))
+
+                    Text(
+                        text = stringResource(id = R.string.help_text),
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    locale = "ru"
+)
+@Composable
+fun HelpScreenPreview() {
+    AstralVPNTheme(darkTheme = true) {
+        HelpScreen(
+            onBack = {}
+        )
+    }
+}
