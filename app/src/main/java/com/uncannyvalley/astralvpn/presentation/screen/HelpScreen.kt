65234127a9248d87f696da2d602776a93f65a9d5
The-uncanny-valley <runcannyvalley@gmail.com>
2025-12-09 15:57:08 +0300

    fix(ui): HelpScreen UI improved

--- app/src/main/java/com/uncannyvalley/astralvpn/presentation/screen/HelpScreen.kt
+++ app/src/main/java/com/uncannyvalley/astralvpn/presentation/screen/HelpScreen.kt
@@ -1,6 +1,8 @@
 package com.uncannyvalley.astralvpn.presentation.screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -16,7 +19,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -70,10 +75,13 @@ fun HelpScreen(
                 ) {
                     Icon(
                         painter = painterResource(R.drawable.btn_return),
-                        contentDescription = null,
+                        contentDescription = "Return",
                         tint = Color.Unspecified,
                         modifier = Modifier
+                            .size(24.dp)
                             .clickable(
+                                interactionSource = remember { MutableInteractionSource() },
+                                indication = ripple(bounded = false),
                                 onClick = onBack
                             )
                     )
