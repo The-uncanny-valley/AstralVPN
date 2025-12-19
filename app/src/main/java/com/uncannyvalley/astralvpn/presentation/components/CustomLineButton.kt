86e72b45a1c0c5bdee9b2379ad5bf7b8cfa0cfe2
The-uncanny-valley <runcannyvalley@gmail.com>
2025-12-19 22:27:05 +0300

    refactor: remove unused imports

--- app/src/main/java/com/uncannyvalley/astralvpn/presentation/components/CustomLineButton.kt
+++ app/src/main/java/com/uncannyvalley/astralvpn/presentation/components/CustomLineButton.kt
@@ -3,12 +3,10 @@ package com.uncannyvalley.astralvpn.presentation.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
