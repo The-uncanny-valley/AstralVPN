bfaeaac3796b161c2cc743033d204cba79945c5a
The-uncanny-valley <runcannyvalley@gmail.com>
2026-01-30 17:53:44 +0300

    feat(ui): replace Column with LazyVerticalGrid for app icon options and update padding and border width

--- app/src/main/java/com/uncannyvalley/astralvpn/presentation/icon/AppIconScreen.kt
+++ app/src/main/java/com/uncannyvalley/astralvpn/presentation/icon/AppIconScreen.kt
@@ -15,6 +15,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
@@ -27,11 +31,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -85,7 +85,6 @@ fun AppIconScreen(
                 Column(
                     modifier = Modifier
                         .fillMaxHeight()
-                        .verticalScroll(rememberScrollState())
                         .clip(RoundedCornerShape(16.dp))
                         .padding(horizontal = 42.dp, vertical = 48.dp),
                     verticalArrangement = Arrangement.Top
@@ -152,12 +151,12 @@ fun AppIconItem(
             modifier = Modifier
                 .size(52.dp)
                 .border(
-                    width = 2.dp,
+                    width = 1.dp,
                     color = if (selected) MaterialTheme.colorScheme.primary
                     else MaterialTheme.colorScheme.outline,
                     shape = RoundedCornerShape(12.dp)
                 )
-                .padding(3.dp)
+                .padding(4.dp)
         ) {
             Image(
                 painter = painterResource(option.imageRes),
@@ -182,11 +181,15 @@ fun AppIconPicker(
     selectedId: String,
     onSelectedChanged: (String) -> Unit
 ) {
-    Row(
-        horizontalArrangement = Arrangement.spacedBy(12.dp),
-        modifier = Modifier.selectableGroup()
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(24.dp),
+        horizontalArrangement = Arrangement.spacedBy(24.dp),
+        modifier = Modifier
+            .selectableGroup()
+            .wrapContentHeight()
     ) {
-        options.forEach { option ->
+        items(options) { option ->
             AppIconItem(
                 option = option,
                 selected = option.id == selectedId,
