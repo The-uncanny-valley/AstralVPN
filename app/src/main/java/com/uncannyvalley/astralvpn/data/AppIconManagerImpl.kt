cbeb3a631a454392cfcd4ce682d3c54ae9b2d9c9
The-uncanny-valley <runcannyvalley@gmail.com>
2026-02-01 19:31:27 +0300

    feat: implement launcher icon switching via activity-alias

---
+++ app/src/main/java/com/uncannyvalley/astralvpn/data/AppIconManagerImpl.kt
@@ -0,0 +1,43 @@
+package com.uncannyvalley.astralvpn.data
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.pm.PackageManager
+import android.util.Log
+import com.uncannyvalley.astralvpn.domain.model.AppIcon
+import com.uncannyvalley.astralvpn.domain.model.AppIconManager
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class AppIconManagerImpl @Inject constructor(
+    @ApplicationContext private val context: Context
+) : AppIconManager {
+
+    override fun setIcon(icon: AppIcon) {
+        val pm = context.packageManager
+        val packageName = context.packageName
+
+        val mapping = mapOf(
+            AppIcon.DEFAULT to "$packageName.LauncherDefault",
+            AppIcon.LIGHT to "$packageName.LauncherLight",
+            AppIcon.CLOCK to "$packageName.LauncherClock",
+            AppIcon.CALC to "$packageName.LauncherCalc"
+        )
+
+        mapping.forEach { (type, alias) ->
+            val newState = if (type == icon) {
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+            } else {
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+            }
+
+            pm.setComponentEnabledSetting(
+                ComponentName(packageName, alias),
+                newState,
+                0
+            )
+
+            Log.d("AppIconManager", "Alias $alias set to $newState")
+        }
+    }
+}
