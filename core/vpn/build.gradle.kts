5cad90b7c8af787b52a603a1b4eb5c9bfe35c013
The-uncanny-valley <runcannyvalley@gmail.com>
2026-03-17 21:35:20 +0300

    feat(vpn): add core:domain dependency

--- core/vpn/build.gradle.kts
+++ core/vpn/build.gradle.kts
@@ -46,6 +46,8 @@ kotlin {
 }
 
 dependencies {
+    implementation(project(":core:domain"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
